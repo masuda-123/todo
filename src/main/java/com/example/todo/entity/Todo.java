3070bbae68a01508eedf7909299bba8f4134f676
masuda <masuda@example.com>
2026-02-15 10:35:24 +0900

    [add]entityクラスを作成

---
+++ src/main/java/com/example/todo/entity/Todo.java
@@ -0,0 +1,33 @@
+package com.example.todo.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Todo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private boolean done;
+	private LocalDateTime createdAt;
+	
+	public Todo() {}
+	
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public boolean isDone() { return done; }
+    public void setDone(boolean done) { this.done = done; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
