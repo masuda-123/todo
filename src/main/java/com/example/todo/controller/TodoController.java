2d22b6e526e1663f474d88eec1999773894aa5c4
masuda <masuda@example.com>
2026-02-15 13:50:47 +0900

    [add]TodoControllerを作成

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,35 @@
+package com.example.todo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.todo.entity.Todo;
+import com.example.todo.service.TodoService;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+	
+	private final TodoService todoService;
+	
+	public TodoController(TodoService todoService) {
+		this.todoService = todoService;
+	}
+	
+    // Todo一覧取得
+    @GetMapping
+    public List<Todo> getTodos() {
+        return todoService.findAll();
+    }
+    
+    // Todo保存
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.save(todo);
+    }
+}
